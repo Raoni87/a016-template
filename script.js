e38777156a5c63417ed1573eb6530fea40efa290
Raoni87 <raonioliva@gmail.com>
2022-08-17 21:36:09 -0300

    exercicios da aula 16

--- script.js
+++ script.js
@@ -34,4 +34,94 @@ const filmes = [
   },
 ]
 
+for (let i = 0; i < filmes.length; i++) {
+  console.log(`O título do filme é: ${filmes[i].titulo}, lançado em ${filmes[i].ano}, dirigido por ${filmes[i].diretor}`);
+  for (let z = 0; z< filmes[i].elenco.length; z++) {
+    console.log(`tem no elenco: ${filmes[i].elenco[0]}, ${filmes[i].elenco[1]}, ${filmes[i].elenco[2]}, ${filmes[i].elenco[4]}`);
+  }
+} 
+
+
+for (let i in filmes) {
+  for (let z of filmes) {
+    console.log(`${z.titulo}, lançado em ${z.ano}, dirigido por ${z.diretor}. Elenco: ${z.elenco}`);
+  }
+}
+
 // escreva seu código abaixo 👇🏻
+
+// const resultado1 = [2, 4, 6, 8];
+// const resultado2 = [1, 3, 5, 7];
+// const resultado3 = [0, 8, 10, 12];
+// const resultado4 = [9, 11, 13, 15];
+
+// const megaSena = [resultado1, resultado2, resultado3, resultado4];
+
+// console.log(megaSena);
+
+// if(megaSena.length === 4) {
+
+
+//   for(let i = 0; i < megaSena.length; i++){
+//       for (let z = 0; z < megaSena[i].length; z++){
+//             console.log(`sorteio ${i +1}: ${megaSena[i]}`)
+//       }
+// } 
+
+
+// } else {
+//     console.log('É necessário alterar o número de itens do array');
+// }
+
+
+//exercicio 2
+
+// const resultado1 = [2, 4, 6, 8];
+// const resultado2 = [1, 3, 5, 7];
+// const resultado3 = [0, 8, 10, 12];
+// const resultado4 = [9, 11, 13, 15];
+
+// const megaSena = [resultado1, resultado2, resultado3, resultado4];
+
+// console.log(megaSena);
+
+// if(megaSena.length === 4) {
+
+
+//   for(let i in megaSena){
+//       for (let z = 0; z < megaSena[i].length; z++){
+//             console.log(`sorteio ${Number(i) + 1}: ${megaSena[i]}`)
+//       }
+// } 
+
+
+// } else {
+//     console.log('É necessário alterar o número de itens do array');
+// }
+
+//exercicio 3
+
+// const resultado1 = [2, 4, 6, 8];
+// const resultado2 = [1, 3, 5, 7];
+// const resultado3 = [0, 8, 10, 12];
+// const resultado4 = [9, 11, 13, 15];
+
+// const megaSena = [resultado1, resultado2, resultado3, resultado4];
+
+// console.log(megaSena);
+
+// if(megaSena.length === 4) {
+
+//   //i é a chave e negaSena é o valor
+//   for(let i in megaSena){
+
+//       //elemento do array
+//       for (let z of megaSena[i]){
+//             console.log(megaSena[i]);
+//       }
+// } 
+
+
+// } else {
+//     console.log('É necessário alterar o número de itens do array');
+// }
